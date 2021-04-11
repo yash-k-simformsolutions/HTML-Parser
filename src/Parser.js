066e9209f17ab28ccb20ef74dce007611be709ea
Yash Khorja <yash.khorja@iMac-Pro-4.local>
2021-04-11 23:13:59 +0530

    completed

--- src/Parser.js
+++ src/Parser.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {htmlContent} from './htmlContent';
 import './Parser.css';
-import axios from 'axios';
 
 var ticket, comment, PCS, multiPCS
 
@@ -13,17 +12,18 @@ var ticket, comment, PCS, multiPCS
 function Parser() {
     var counter = 0
     console.log("Re-rendering->>>>>>>>>>>")
+
     const parser = () => {
         const parser = new DOMParser()
         const parsedHtml = parser.parseFromString(htmlContent, 'text/html')
         const content = parsedHtml.getElementsByClassName('ghx-row')
         const data = [...content]
         
-        return data.map((item) => {
+        return data.map((item, index) => {
             ticket = item.childNodes[4].children[0].childNodes[0].data
             comment = item.childNodes[6].children[0].childNodes[0].data
             
-            if (comment.includes("Blocked")) {
+            if (comment.includes("Blocked") && comment.includes("PCS")) {
                 PCS = (comment).substring((comment).indexOf("Blocked") + 11).trim()
                 // console.log("Comment->>>>>", comment)
                 // console.log(PCS.split(","))
@@ -35,59 +35,37 @@ function Parser() {
                         <td>{ticket}</td>
                         <td>{comment}</td>
                         <td>{ multiPCS.map((pcs) => <a href={`https://dev.pulsesecure.net/jira/browse/${pcs}`}>{pcs}</a>) }</td>
-                        <td>NA</td>
-                        {/* <td>{ multiPCS.map((pcs) => getStatus(`https://dev.pulsesecure.net/jira/browse/${pcs}`)) }</td> */}
+                        {/* <td>NA</td> */}
+                        {/* {<td>{getStatus()}</td>} */}
+                        <td>{ multiPCS.map((pcs) => getStatus(`https://dev.pulsesecure.net/jira/browse/${pcs}`)) }</td>
                     </tr>
                 )
             }
         })
     }
-    // const getStatus = (url) => {
-    //     console.log(url)
-    //     const response = axios.get(url).then((res) => console.log(res.status))
-    //     console.log(response)
-        
-        // var status
-        // // console.log("Inside getStatus->>>>>>>")
-        // // console.log(url)
-        // // const url=`https://jsonplaceholder.typicode.com/todos/1`
-        // const HTTP = new XMLHttpRequest()
-        // HTTP.open("GET",url)
-        // HTTP.send(null)
-        // HTTP.onreadystatechange = () => {
-        //     if(HTTP.readyState===4){
-        //         if(HTTP.status===200){
-        //             status = "Available"
-        //             console.log(status)
-        //         }
-        //         else{
-        //             status = "404"
-        //             console.log(status)
-        //         }
-        //     }
-        //     console.log(url)
-        //     return {status}
-        // }
-    // }
     
-    // function getStatus(url) {
-    //     var request = new XMLHttpRequest();
-    //     request.onreadystatechange = function() {
-    //         if (request.readyState === 4){
-    //             // request.status;//this contains the status code
-    //             setHtml(request.status);
-    //         }
-    //     };
-    //         request.open("GET", url, true);
-    //         request.send(); 
-    // } 
-
-    // function setHtml(htmldata) {
-    //     console.log(htmldata);
-    //     return htmldata
-    //     // var theDiv = document.getElementById(targetC);
-    //     // theDiv.innerHTML = htmldata;
-    // }
+    const getStatus = (url) => {
+        // const url = 'https://jsonplaceholder.typicode.com/users';
+        console.log(url)
+        var flag = 0;
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = () => {
+            if (request.readyState === 4){
+                console.log(request.status);
+                if (request.status === 200){
+                    console.log("Successful!")
+                    flag = 1;
+                }
+                else{
+                    console.log("Error!")
+                    flag = 0;
+                }
+            }
+        };
+        request.open("GET", url, true);
+        request.send(); 
+        return flag === 1 ? "Successful!" : "Error!"
+    } 
 
     return (
         <div className="parser">
@@ -103,7 +81,7 @@ function Parser() {
                     { parser() }
                 </tbody>
             </table>
-            {counter}
+            <h3>Total {counter} blocked tickets are detected!</h3>
         </div>
     )
 }
